c64037ce2d5bdb356126ac9bc40ee2b2897623f1
Neo Zhi Xuan <neozhixuan@gmail.com>
2021-07-14 15:04:31 +0800

    ..

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+NUM_WORKERS=3
+TIMEOUT=120
+
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+--name $NAME \
+--workers $NUM_WORKERS \
+--timeout $TIMEOUT \
+--log-level=debug \
+--bind=127.0.0.1:9000 \
+--pid=$PIDFILE
